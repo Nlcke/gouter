8fbd91d6f88cb5847b2cfc61683bb92d6768eea2
nlcke <n1cke@mail.ru>
2021-08-14 23:59:51 +0300

    add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'error',
+    'dot-notation': 'off',
+    'no-shadow': 'off',
+    'no-unused-vars': 'off',
+  },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+/** @type {Parameters<import('prettier')['format']>[1]} */
+module.exports = {
+  printWidth: 100,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  arrowParens: 'always',
+  proseWrap: 'always',
+  tabWidth: 2,
+  useTabs: false,
+  quoteProps: 'consistent',
+  jsxSingleQuote: false,
+  rangeStart: 0,
+  endOfLine: 'auto',
+};
