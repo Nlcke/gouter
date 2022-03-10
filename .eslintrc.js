41edc6fc71b2785b516c541227dba6abfdbeca83
nlcke <n1cke@mail.ru>
2022-03-11 00:11:12 +0300

    .eslintrc: use only @react-native-community

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,4 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
-  rules: {
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'error',
-    'dot-notation': 'off',
-    'no-shadow': 'off',
-    'no-unused-vars': 'off',
-  },
 };
